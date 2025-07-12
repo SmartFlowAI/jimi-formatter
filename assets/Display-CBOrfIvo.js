f9dc99366a28efcefa7f6827051662afd28ea9b9
Howardzhangdqs <75195784+Howardzhangdqs@users.noreply.github.com>
2025-07-12 13:09:22 +0000

    Deploying to gh-pages from @ SmartFlowAI/jimi-formatter@ef58395e5c0d7bd255f8e17686a79fa5eef0a9a0 🚀

---
+++ assets/Display-CBOrfIvo.js
@@ -0,0 +1,276 @@
+import{d as en,r as B,l as tn,m as on,e as D,c as v,F as rn,w as an,f as E,n as j,u as M,p as sn,t as ln,q as cn,a as P,s as dn,x as mn,o as T,j as fn,_ as gn}from"./index-C1-lMrFi.js";import{g as un,r as pn,M as hn}from"./vendor-markdown-CUnfxHn9.js";import{H as $}from"./vendor-highlight-BAUc8pQu.js";const bn=`.md h1 {
+    font-size: larger;
+    font-weight: bold;
+}
+
+.md h2 {
+    font-size: large;
+    font-weight: bold;
+}
+
+.md h3 {
+    font-size: medium;
+    font-weight: bold;
+}
+
+.md h4 {
+    font-size: small;
+    font-weight: bold;
+}
+
+.md h5 {
+    font-size: smaller;
+    font-weight: bold;
+}
+
+.md h6 {
+    font-size: x-small;
+    font-weight: bold;
+}
+
+.md a {
+    color: #4007a2;
+}
+
+.md .link {
+    color: #4007a2;
+    white-space: break-spaces;
+}
+
+.md .reference-container {
+    display: flex;
+    font-size: 14px;
+    line-height: 1.8em;
+    letter-spacing: 0em;
+}
+
+.md .reference-index {
+    line-height: 1.8em;
+    letter-spacing: 0em;
+    color: rgba(0, 0, 0, 0.6);
+    display: inline;
+    width: 3em;
+    min-width: 3em;
+    background-image: none;
+    background-position: initial;
+    background-size: initial;
+    background-repeat: initial;
+    background-attachment: initial;
+    background-origin: initial;
+    background-clip: initial;
+    font-size: 80%;
+    font-family: ptima-Regular, Optima, PingFangSC-light, PingFangTC-light, "PingFang SC", Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+    padding-top: 2px;
+}
+
+.md .reference-title {
+    font-family: Georgia, "Microsoft YaHei", PingFangSC-regular, serif;
+    margin: 0;
+}
+
+.md .reference-href {
+    font-family: Georgia, "Microsoft YaHei", PingFangSC-regular, serif;
+    font-style: italic;
+}
+
+.md code {
+    font-family: 'Roboto Mono', Consolas, Monaco, Menlo, monospace;
+    font-size: 0.9em;
+    background-color: #f8f8f8;
+    padding: 0.2em 0.4em;
+    border-radius: 0.3em;
+}
+
+.md pre code {
+    font-family: 'Roboto Mono', Consolas, Monaco, Menlo, monospace;
+    font-size: 0.9em;
+    background-color: #31363f !important;
+    padding: 0.2em 0.4em;
+    border-radius: 0.3em;
+}
+
+.md pre {
+    max-width: 100%;
+    font-family: 'Roboto Mono', Consolas, Monaco, Menlo, monospace;
+    font-size: 0.9em;
+    background-color: #31363f;
+    color: white;
+    padding: .8em 1em;
+    border-radius: 0.3em;
+    white-space: break-spaces;
+}
+
+.md .mathjax-display {
+    direction: ltr;
+    display: block;
+    text-align: center;
+    margin: 1em 0;
+    position: relative;
+}
+
+.md blockquote {
+    max-width: 100%;
+    /* width: 100%; */
+}
+
+.markdown>p {
+    margin-block-start: 1em !important;
+    margin-block-end: 1em !important;
+    margin-inline-start: 0px !important;
+    margin-inline-end: 0px !important;
+}
+
+.md .underline {
+    text-decoration: underline !important;
+    text-decoration-style: dashed !important;
+    text-underline-offset: .1em;
+    text-decoration-thickness: 1px !important;
+}
+
+.md figcaption {
+    font-size: 0.8em;
+    color: #666;
+    text-align: center;
+    /* margin-top: 0.5em; */
+}`,xn=`.markdown {
+    font-family: Georgia, "Microsoft YaHei", PingFangSC-regular, serif;
+}
+
+.markdown h1 {
+    font-size: 22px;
+    color: rgb(237, 74, 39);
+    line-height: 1.5em;
+    letter-spacing: 0em;
+    padding-top: 20px;
+    align-items: center;
+    background-attachment: scroll;
+    background-clip: border-box;
+    background-image: none;
+    background-origin: padding-box;
+    background-position: 0% 0%;
+    background-repeat: no-repeat;
+    background-size: auto;
+    border-style: none;
+    border-width: 1px;
+    border-color: rgb(0, 0, 0);
+    border-radius: 0px;
+    box-shadow: none;
+    display: flex;
+    font-weight: bold;
+    flex-direction: column;
+    float: unset;
+    height: auto;
+    justify-content: center;
+    overflow: unset;
+    text-indent: 0em;
+    text-shadow: none;
+    transform: none;
+    width: auto;
+    -webkit-box-reflect: unset;
+    visibility: visible;
+}
+
+.markdown h2 {
+    margin-top: 30px;
+    margin-bottom: 15px;
+    line-height: 1.5em;
+    overflow: unset;
+    text-shadow: none;
+    transform: none;
+    width: auto;
+    -webkit-box-reflect: unset;
+    visibility: visible;
+    font-size: 18px;
+    color: rgb(34, 34, 34);
+    line-height: 1.8em;
+    letter-spacing: 0em;
+    padding-left: 10px;
+    border-style: none none none solid;
+    border-width: 1px 1px 1px 5px;
+    border-color: rgb(0, 0, 0) rgb(0, 0, 0) rgb(0, 0, 0) rgb(248, 57, 41);
+    border-radius: 0px;
+    background-attachment: scroll;
+    background-clip: border-box;
+    background-image: none;
+    background-origin: padding-box;
+    background-position: 0% 0%;
+    background-repeat: no-repeat;
+    background-size: auto;
+    box-shadow: none;
+    display: block;
+    font-weight: bold;
+}
+
+.md a {
+    color: rgb(18, 142, 255);
+    text-decoration: none;
+}
+
+.md .link {
+    color: rgb(18, 142, 255);
+    text-decoration: none;
+}
+
+.md .reference-container {
+    display: flex;
+    font-size: 14px;
+    line-height: 1.8em;
+    letter-spacing: 0em;
+}
+
+.md .reference-index {
+    line-height: 1.8em;
+    letter-spacing: 0em;
+    color: rgba(0, 0, 0, 0.6);
+    display: inline;
+    width: 3em;
+    background-image: none;
+    background-position: initial;
+    background-size: initial;
+    background-repeat: initial;
+    background-attachment: initial;
+    background-origin: initial;
+    background-clip: initial;
+    font-size: 80%;
+    font-family: ptima-Regular, Optima, PingFangSC-light, PingFangTC-light, "PingFang SC", Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+    padding-top: 2px;
+}
+
+.md .reference-title {
+    font-family: Georgia, "Microsoft YaHei", PingFangSC-regular, serif;
+    /* padding-right: .5em; */
+}
+
+.md .reference-href {
+    font-family: Georgia, "Microsoft YaHei", PingFangSC-regular, serif;
+}
+
+.md blockquote {
+    border-left: 3px solid #DBDBDB;
+    color: rgba(0, 0, 0, 0.55);
+    font-size: 15px;
+    text-indent: 0;
+    margin: 0;
+    padding: .5em 1em .3em 1em;
+}
+
+.md strong {
+    font-weight: bold;
+    color: red;
+}
+
+.md figure {
+    margin-left: 0;
+    margin-right: 0;
+}
+
+.md figure img {
+    max-width: 100%;
+    width: 100%;
+    height: auto;
+    margin-left: 0;
+    margin-right: 0;
+}`,q=n=>`
+${n}
+`,yn={default:q(bn),weekly:q(xn)};var _n=pn();const wn=un(_n),x=typeof Buffer=="function",R=typeof TextDecoder=="function"?new TextDecoder:void 0,U=typeof TextEncoder=="function"?new TextEncoder:void 0,kn="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",y=Array.prototype.slice.call(kn),A=(n=>{let e={};return n.forEach((l,m)=>e[l]=m),e})(y),An=/^(?:[A-Za-z\d+\/]{4})*?(?:[A-Za-z\d+\/]{2}(?:==)?|[A-Za-z\d+\/]{3}=?)?$/,s=String.fromCharCode.bind(String),G=typeof Uint8Array.from=="function"?Uint8Array.from.bind(Uint8Array):n=>new Uint8Array(Array.prototype.slice.call(n,0)),Cn=n=>n.replace(/=/g,"").replace(/[+\/]/g,e=>e=="+"?"-":"_"),W=n=>n.replace(/[^A-Za-z0-9\+\/]/g,""),vn=n=>{let e,l,m,u,g="";const p=n.length%3;for(let h=0;h<n.length;){if((l=n.charCodeAt(h++))>255||(m=n.charCodeAt(h++))>255||(u=n.charCodeAt(h++))>255)throw new TypeError("invalid character found");e=l<<16|m<<8|u,g+=y[e>>18&63]+y[e>>12&63]+y[e>>6&63]+y[e&63]}return p?g.slice(0,p-3)+"===".substring(p):g},Y=typeof btoa=="function"?n=>btoa(n):x?n=>Buffer.from(n,"binary").toString("base64"):vn,En=x?n=>Buffer.from(n).toString("base64"):n=>{let l=[];for(let m=0,u=n.length;m<u;m+=4096)l.push(s.apply(null,n.subarray(m,m+4096)));return Y(l.join(""))},Mn=n=>{if(n.length<2){var e=n.charCodeAt(0);return e<128?n:e<2048?s(192|e>>>6)+s(128|e&63):s(224|e>>>12&15)+s(128|e>>>6&63)+s(128|e&63)}else{var e=65536+(n.charCodeAt(0)-55296)*1024+(n.charCodeAt(1)-56320);return s(240|e>>>18&7)+s(128|e>>>12&63)+s(128|e>>>6&63)+s(128|e&63)}},Tn=/[\uD800-\uDBFF][\uDC00-\uDFFFF]|[^\x00-\x7F]/g,Fn=n=>n.replace(Tn,Mn),I=x?n=>Buffer.from(n,"utf8").toString("base64"):U?n=>En(U.encode(n)):n=>Y(Fn(n)),N=(n,e=!1)=>e?Cn(I(n)):I(n),Sn=/[\xC0-\xDF][\x80-\xBF]|[\xE0-\xEF][\x80-\xBF]{2}|[\xF0-\xF7][\x80-\xBF]{3}/g,Ln=n=>{switch(n.length){case 4:var e=(7&n.charCodeAt(0))<<18|(63&n.charCodeAt(1))<<12|(63&n.charCodeAt(2))<<6|63&n.charCodeAt(3),l=e-65536;return s((l>>>10)+55296)+s((l&1023)+56320);case 3:return s((15&n.charCodeAt(0))<<12|(63&n.charCodeAt(1))<<6|63&n.charCodeAt(2));default:return s((31&n.charCodeAt(0))<<6|63&n.charCodeAt(1))}},Hn=n=>n.replace(Sn,Ln),zn=n=>{if(n=n.replace(/\s+/g,""),!An.test(n))throw new TypeError("malformed base64.");n+="==".slice(2-(n.length&3));let e,l="",m,u;for(let g=0;g<n.length;)e=A[n.charAt(g++)]<<18|A[n.charAt(g++)]<<12|(m=A[n.charAt(g++)])<<6|(u=A[n.charAt(g++)]),l+=m===64?s(e>>16&255):u===64?s(e>>16&255,e>>8&255):s(e>>16&255,e>>8&255,e&255);return l},Z=typeof atob=="function"?n=>atob(W(n)):x?n=>Buffer.from(n,"base64").toString("binary"):zn,Bn=x?n=>G(Buffer.from(n,"base64")):n=>G(Z(n).split("").map(e=>e.charCodeAt(0))),Dn=x?n=>Buffer.from(n,"base64").toString("utf8"):R?n=>R.decode(Bn(n)):n=>Hn(Z(n)),jn=n=>W(n.replace(/[-_]/g,e=>e=="-"?"+":"/")),Nn=n=>Dn(jn(n)),Pn={class:"button-container"},$n=["innerHTML"],qn=["innerHTML"],Rn=en({__name:"Display",props:{embed_mode:{type:Boolean},show_copy_button:{type:Boolean},show_embed_button:{type:Boolean}},setup(n){const e=n,{show_copy_button:l,show_embed_button:m,embed_mode:u}=e,g=hn({html:!0,highlight:(r,a)=>{if(a&&$.getLanguage(a))try{return $.highlight(r,{language:a}).value}catch{}return""}}).use(wn),p=B(""),h=["mp.weixin.qq.com"],O=r=>{const a=/\s"((?:[^"\\]|\\["\\])*)"$/g,c=r.match(a);return c?[r.replace(c[0],""),c[0].slice(2,-1)]:[r,r]};tn(async()=>{const r=g.render(mn(P.value)),a=document.createElement("div");a.innerHTML=r,a.querySelectorAll("img").forEach(t=>{var i;if((t.getAttribute("alt")||"")==="")return;if(((i=t.parentElement)==null?void 0:i.tagName)==="P"){const f=document.createElement("figure");f.classList.add("image"),f.innerHTML=t.outerHTML;const k=document.createElement("figcaption");k.innerHTML=t.getAttribute("alt")||"",f.appendChild(k),t.parentElement.replaceWith(f);return}const o=document.createElement("figure");o.classList.add("image"),o.innerHTML=t.outerHTML;const d=document.createElement("figcaption");d.innerHTML=t.getAttribute("alt")||"",o.appendChild(d),t.replaceWith(o)}),a.querySelectorAll("mjx-container").forEach(t=>{const o=t.querySelector("svg");if(o)if(t.getAttribute("display")==="true"){const d=o.getAttribute("width");d&&(o.style.width=`${parseFloat(d.replace("ex",""))/2}em`);const i=o.getAttribute("height");i&&(o.style.height=`${parseFloat(i.replace("ex",""))/2}em`),o.removeAttribute("width"),o.removeAttribute("height");const f=document.createElement("p");f.classList.add("mathjax-display"),f.classList.add("jimi-"),f.innerHTML=o.outerHTML,t.insertAdjacentElement("afterend",f),t.remove()}else t.insertAdjacentElement("afterend",o),t.remove();else t.remove()}),a.querySelectorAll("u").forEach(t=>{const o=document.createElement("span");o.innerHTML=t.innerHTML,o.classList.add("underline");const d=t.attributes;for(let i=0;i<d.length;i++)o.setAttribute(d[i].name,d[i].value);t.replaceWith(o)});const X=a.querySelectorAll("a"),w=[],C=[];X.forEach(t=>{if(h.some(o=>t.href.includes(o))){t.setAttribute("target","_blank");return}C.push(t)}),C.forEach(t=>{const o=t.getAttribute("href")||"",d=t.innerText;for(const H of w)if(H.href===o){const z=document.createElement("sup");z.innerHTML=`<span class="link" href="${o}" target="_blank">[${H.index}]</span>`,t.insertAdjacentElement("afterend",z);return}const i=w.length+1,[f,k]=O(d);w.push({href:o,title:f,explain:k,index:i}),t.innerText=f,t.removeAttribute("href");const L=document.createElement("sup");L.innerHTML=`<span class="link" href="${o}" target="_blank">[${i}]</span>`,t.insertAdjacentElement("afterend",L)}),C.forEach(t=>{const o=document.createElement("span");o.innerHTML=t.innerHTML;const d=t.attributes;for(let i=0;i<d.length;i++)o.setAttribute(d[i].name,d[i].value);o.classList.add("link"),t.replaceWith(o)});const S=w.map(t=>[`<span class="reference-container"><span class="reference-index">[${t.index}]</span>`,`<p class="reference-title">${t.explain}：`,`<span class="reference-href link">${t.href}</span></p></span>`].join("")).join(`
+`);p.value=`${a.innerHTML}`+(S.length>0?`<h1>参考资料</h1><section>${S}</section>`:"");const nn=document.createElement("div");nn.innerHTML=p.value});const V=/\@import\((.*?)\)/g,J=on(()=>{let r=D.value;const a=r.match(V);return a&&a.forEach(c=>{const _=c.slice(8,-1).split(",");r=r.replace(c,_.map(F=>yn[F.trim()]).join(""))}),`<style>${r}</style>`}),b=B(0),K=()=>{try{const r=N(P.value),a=N(D.value),c=new URLSearchParams;c.set("content",r),c.set("style",a);const _=dn.resolve({name:"embed",query:Object.fromEntries(c)}).href;window.open(_,"_blank")}catch(r){console.error("Error encoding content for embed:",r),alert("编码内容时出错，请检查内容是否包含特殊字符")}},Q=()=>{const r=window.getSelection(),a=document.createRange(),c=document.querySelector(".markdown");c&&a.selectNodeContents(c),r&&(r.removeAllRanges(),r.addRange(a),document.execCommand("copy"),r.removeAllRanges(),b.value=2e3)};return setInterval(()=>{b.value>0?b.value-=100:b.value=0},100),(r,a)=>(T(),v(rn,null,[an(E("div",Pn,[M(l)!==!1?(T(),v("button",{key:0,onClick:Q,class:"btn",style:sn({color:b.value?"rgb(0, 146, 0)":"black"})},ln(b.value?"复制成功":"一键复制"),5)):j("",!0),M(m)!==!1?(T(),v("button",{key:1,onClick:K,class:"btn embed-btn"}," 嵌入模式 ")):j("",!0)],512),[[fn,!0]]),E("div",{innerHTML:p.value,class:cn(["markdown md",M(u)?"main-display":""])},null,10,$n),E("div",{innerHTML:J.value},null,8,qn)],64))}}),Wn=gn(Rn,[["__scopeId","data-v-85fedf5f"]]);export{Wn as D,Nn as d};
