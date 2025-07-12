e6a804d5f50f60b004e088c7c1736b682fda6f4f
HowardZhangdqs <zjh@shanghaiit.com>
2025-07-12 19:49:09 +0800

    feat: Add Vue Router configuration to support HomeView and EmbedView.

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import EmbedView from '../views/EmbedView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/embed',
+      name: 'embed',
+      component: EmbedView
+    }
+  ]
+})
+
+export default router
