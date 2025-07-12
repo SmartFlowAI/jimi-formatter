2022f76cdf1ac6400ce4310a0c3f8ce84edde3f7
HowardZhangdqs <zjh@shanghaiit.com>
2025-07-12 20:29:59 +0800

    :bug: Fix router history mode to use createWebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import EmbedView from '../views/EmbedView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
